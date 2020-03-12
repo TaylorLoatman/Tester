55bebb3c24217c9b38a3aa6cf2ee9dc239ce344a
TaylorLoatman <60367041+TaylorLoatman@users.noreply.github.com>
2020-03-12 14:46:19 -0400

    Initial Commit

---
+++ Tester/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tester
+//
+//  Created by Taylor Loatman on 3/12/20.
+//  Copyright © 2020 Taylor Loatman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
